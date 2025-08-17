9563e32808bd34c341d827d1b7d3b684c0abf920
Ernest Guevarra <ernest@guevarra.io>
2025-08-17 17:47:51 +0100

    setup repository for pdf downloads

--- R/access_googledrive.R
+++ R/access_googledrive.R
@@ -1,53 +1,28 @@
 #'
+#' Get Google Sheets ID
+#' 
+
+get_gsheets_id <- function(path, filename) {  
+  id <- googledrive::drive_ls(
+    path = path, pattern = filename, recursive = TRUE
+  ) |>
+    dplyr::pull(id)
+
+  id
+}
+
+
+#'
 #' Download files from Google Drive
 #'
 
-download_googledrive <- function(filename, 
-                                 path = paste0("data/", filename),
-                                 overwrite = FALSE) {
-  ## Authenticate
-  googledrive::drive_auth(
-    email = Sys.getenv("GOOGLE_AUTH_EMAIL"),
-    path = Sys.getenv("GOOGLE_AUTH_FILE")
-  )
-  
+download_gdrive <- function(filename, 
+                            path,
+                            overwrite = FALSE) {  
   ## 
   googledrive::drive_download(
     file = filename,
     path = path,
     overwrite = overwrite
   )
-}
-
-
-#'
-#' Access Google Sheets
-#'
-
-get_googlesheets <- function(id = std_data_id) {
-  googlesheets4::gs4_auth(
-    email = Sys.getenv("GOOGLE_AUTH_EMAIL"),
-    path = Sys.getenv("GOOGLE_AUTH_FILE")
-  )
-  
-  googlesheets4::read_sheet(
-    ss = id, sheet = 1
-  )
-}
-
-
-get_googlesheets_id <- function(filename) {
-  ## Authenticate
-  googledrive::drive_auth(
-    email = Sys.getenv("GOOGLE_AUTH_EMAIL"),
-    path = Sys.getenv("GOOGLE_AUTH_FILE")
-  )
-  
-  files <- googledrive::drive_find()
-  
-  id <- files |>
-    subset(name == filename, select = id) |>
-    as.character()
-
-  id
 }
--- _targets.R
+++ _targets.R
@@ -10,11 +10,28 @@ for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 
 ### Google deauthorisation ----
 googledrive::drive_deauth()
+googlesheets4::gs4_deauth()
 
 
 ## Data targets ----
 data_targets <- tar_plan(
-  
+  ## URL for himsog directory in Google Drive ----
+  himsog_gdrive_directory_id = googledrive::as_id(
+    "https://drive.google.com/drive/folders/1BP9eXlBo3dpX0c9itcbwQe9OSSZcUm2w"
+  ),
+  tar_target(
+    name = himsog_gdrive_file_list_id,
+    command = get_gsheets_id(
+      path = himsog_gdrive_directory_id, 
+      filename = "doh_disease_surveillance_files"
+    )
+  ),
+  tar_target(
+    name = himsog_edcs_gdrive_files,
+    command = googlesheets4::read_sheet(
+      ss = himsog_gdrive_file_list_id, sheet = 1
+    )
+  )
 )
 
 
