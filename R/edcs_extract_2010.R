1d27bbb8c0f24ee7c6ad98693c5f479c0a27068c
Ernest Guevarra <ernest@guevarra.io>
2025-08-21 07:40:09 +0100

    extract 2010 edcs; fix #41

--- R/edcs_extract_2010.R
+++ R/edcs_extract_2010.R
@@ -3,10 +3,21 @@
 #' 
 
 edcs_extract_data_2010 <- function(gdrive, edcs_disease_list) {
+  x <- c(
+    "cases_cum_week51", "deaths_cum_week51", 
+    "cases_n_week52", "deaths_n_week52"
+  )
+
+  df_names <- lapply(
+    X = edcs_disease_list$disease_code, FUN = paste, x, sep = "_"
+  ) |>
+    unlist()
+  df_names <- c("region", df_names)
+
   destfile <- file.path(tempdir(), gdrive$name)
   googledrive::drive_download(file = gdrive, path = destfile, overwrite = TRUE)
 
-  pdf_data <- pdftools::pdf_ocr_text(destfile) |>
+  df <- pdftools::pdf_ocr_text(destfile) |>
     stringr::str_split(pattern = "\n") |>
     lapply(
       FUN = function(x) {
@@ -21,21 +32,64 @@ edcs_extract_data_2010 <- function(gdrive, edcs_disease_list) {
       }
     ) |>
     do.call(cbind, args = _) |>
-    tibble::as_tibble()
-    
-  x <- c(
-    "cases_cum_week_51", "deaths_cum_week_51", 
-    "cases_n_week_52", "deaths_n_week_52"
-  )
+    (\(x) x[ , c(1:13, 15:26, 28:39, 41:52, 54:65, 67:78, 80:91, 93:96, 98:101)])() |>
+    data.frame() |>
+    setNames(nm = df_names) |>
+    tibble::as_tibble() |>
+    dplyr::mutate(
+      dplyr::across(
+        .cols = dplyr::everything(),
+        .fns = function(x) {
+          dplyr::case_when(
+            x %in% c("if", "1.", "a:", "£", ":", "i.", "d,", "dt:", "ic") ~ "1",
+            x %in% c("6)", ")", "8)", "Os") ~ "0",
+            x %in% c("a2", "SZ") ~ "32",
+            x == "ee" ~ "5917",
+            x == "aL" ~ "51",
+            x == "2/" ~ "27",
+            x %in% c("a.", "S") ~ "3",
+            x == "Bi," ~ "11",
+            x == "L9" ~ "15",
+            x == "TL" ~ "77",
+            .default = x
+          )
+        }
+      ),
+      ili_deaths_cum_week51 = ifelse(
+        ili_deaths_cum_week51 == "=", 5, ili_deaths_cum_week51
+      ),
+      pts_cases_cum_week51 = ifelse(
+        pts_cases_cum_week51 == "=", 4, pts_cases_cum_week51
+      ),
+      aes_deaths_cum_week51 = ifelse(
+        aes_deaths_cum_week51 == "Z", 2, aes_deaths_cum_week51
+      ),
+      tys_deaths_cum_week51 = ifelse(
+        tys_deaths_cum_week51 == "Z", 7, tys_deaths_cum_week51
+      )
+    ) |>
+    dplyr::mutate(
+      dplyr::across(.cols = !region, .fns = function(x) as.integer(x))
+    ) |>
+    tidyr::pivot_longer(
+      cols = !region, 
+      names_to = c("disease_code", "type1", "type2", "week"), names_sep = "_",
+      values_to = "n"
+    ) |>
+    dplyr::left_join(
+      y = edcs_disease_list |>
+        dplyr::select(disease_code, disease),
+      by = "disease_code"
+    ) |>
+    dplyr::mutate(
+      week = stringr::str_remove_all(string = week, pattern = "week"),
+      type2 = ifelse(type2 == "cum", "cumulative", "current week")
+    ) |>
+    dplyr::relocate(disease, .after = disease_code)
 
-  df_names <- lapply(
-    X = edcs_disease_list$disease_code, FUN = paste, x, sep = "_"
-  ) |>
-    unlist()
+  unlink(destfile)
 
-  names(pdf_data) <- df_names
-
-  pdf_data
+  df
 }
 
 
